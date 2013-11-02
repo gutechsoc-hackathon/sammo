b05ee042a108332d8d1f6033dac4466932468c88
Michael McDonald <Michael@eduroam44-185.cent.gla.ac.uk>
2013-11-02 15:47:58 +0000

    fixed application.py

--- application.py
+++ application.py
@@ -3,17 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	if request.method == 'POST':
-
-    return 	'''
-        <form action="" method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    '''
+	print 'Hello World'
 
 if __name__ == '__main__':
     app.run()
-
-def createSession(apikey, country, currency, locale, originPlace, destinationPlace, outboundDate,
-					inboundDate = "", locationSchema = "", cabinClass = "", adults = "", children = "", infants = ""):
