5860b49850e759add155bbc0b90d3ef4dbb4a6e7
Andrew McDonald <mcdonalda1993@gmail.com>
2013-11-02 17:12:04 +0000

    added comments to function inCountry

--- helpers.py
+++ helpers.py
@@ -7,58 +7,84 @@ def randFlight(flightIDs):
 #Expects a country, the dictionary and a list
 def inCountry(country, livePriceResponse, itineraries):
 	
+	#Start index of the cities type and country types
 	cityStart, countryStart = findCityCountryStart(livePriceResponse)
 
+	#for each itineraries currently being considered
 	for i in range(len(itineraries)):
+		#outboud has the outbound ID
 		outbound = livePriceResponse['Itineraries'][i]['OutboundLegId']
 
+		##### FIND THE DESTINATION #####
+		#temp is a counter
 		temp = 0
+		#Loop through the legs until it matches the outbound ID
 		while(livePriceResponse['Legs'][temp]['Id'] != outbound
 		       and temp<len(livePriceResponse['Legs'])):
 			temp++
-		
+		#Error checking, will trigger if you have reached end of loop
+		#and you haven't found the leg
 		if(livePriceResponse['Legs'][temp]['Id'] != outbound):
 			print "Error: inCountry, Legs"
 			return
 
+		##### FIND THE AIRPORT ##### 
+		#Gets the destination ID from the leg you found
 		destination = livePriceResponse['Legs'][temp]['DestinationStation']
+		#Reset counter
 		temp = 0
+		#Loops through the places(airports) until you find the destination ID
 		while(livePriceResponse['Places'][temp]['Id'] != destination
-		       and temp<len(livePriceResponse['Places'])):
+		       and temp<cityStart):
 			temp++
-
+		#Error checking, will trigger if you reach end of loop
+		#and you haven't found the destination
 		if(livePriceResponse['Places'][temp]['Id'] != destination):
 			print "Error: inCountry, Places finding initial Airport"
 			return
 
+		##### FIND THE CITY ##### 
+		#Grab the parent from the airport, which is the city
 		cityID = livePriceResponse['Places'][temp]['ParentId']
+		#starts the counter for searching places from where cities begin
 		temp = cityStart
+		#Loop through the places(cities) until you find the cityID
 		while(livePriceResponse['Places'][temp]['Id'] != cityID
-		      and temp<len(livePriceResponse['Places'])):
+		      and temp<countryStart):
 			temp++
-
+		#Error checking, will trigger if you reach end of loop
+		#and you haven't found city
 		if(livePriceResponse['Places'][temp]['Id'] != cityID):
 			print "Error: inCountry, Places finding city"
 			return
 
+		##### FIND THE COUNTRY ##### 
+		#Grab the parent from the city, which is the country
 		countryID = livePriceResponse['Places'][temp]['ParentId']
+		#Starts the counter for searcgubg places from where countries begin
 		temp = countryStart
+		#Loop through the countries until you find the countryID
 		while(livePriceResponse['Places'][temp]['Id'] != countryID
 		      and temp<len(livePriceResponse['Places'])):
 			temp++
-
+		#Error checking, will trigger if you reach end of loop
+		#and you haven't found country
 		if(livePriceResponse['Places'][temp]['Id'] != countryID):
 			print "Error: inCountry, Places finding country"
 			return		
 
+		##### COMPARE THE CURRENT ITINERARIES COUNTRY WITH USER'S ##### 
+		#Grab the country name
 		countryName = livePriceResponse['Places'][temp]['Name']
+		#If the itinerary country doesnt match the user specified country
+		#Remove it from the list
 		if(country != countryName):
-			list[i] = []
+			itineraries[i] = []
 
 	return itineraries
 
 #Expects a min/max range, the dictionary and a list
-def inPriceRange(min, max, dictionary, list):
+def inPriceRange(min, max, livePriceResponse, itineraries):
 
 	return list
 
