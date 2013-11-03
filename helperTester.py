846caf820487b390eb5b947e1defff679684e37c
Michael McDonald <mcdonaldm1993@gmail.com>
2013-11-03 00:38:48 +0000

    changed helperTester to find an airport that has an actual flight and then print its price

--- helperTester.py
+++ helperTester.py
@@ -1,49 +1,51 @@
 import json, helpers, random
 
-
-## get country data
-## get home country from user
-## get currency from user
-## get leaving airport from user
-## get country traveling to from user
-## get leaving date from user
+#
+## get input from user
+## get cities in country
 ## choose a random city from country
-## open the session with data for that city
-## get the data
-## get the cheapest flight for each airport in that city
-## output the data
+## get airport IATA codes in city
+## for each airport
+## 		open the session with data for that city
+## 		get the data
+##		if there are no flights available
+##			remove IATA code from list of airports
+##		else
+## 			get the cheapest flight for airport
+## output the list of airports and cheapest flight data
+#
 
-countryData = 0 ##Pretend this is the json of country:cities
 homeCountry = 'GB'
 homeCurrency = 'GBP'
 leavingAirport = 'gla'
 country = 'UK'
-leavingDate = '2013-12-06'
-##randomCity = randFlight(countryData[country])
-
-#session = helpers.createSession(homeCountry, homeCurrency, leavingAirport, 'Man', leavingDate, inboundDate='2013-12-07')
-# response = helpers.getLivePriceResponse(session)
-# print session+'\n'
-# print response
-# print session+'\n'
+leavingDate = '2013-11-09'
+inboundDate = '2013-11-16'
 
 f = open('airportJSON.txt', 'r')
 fileString = f.read()
 f.close()
 airports = json.loads(fileString)
 
-#print random.choice(airports['uk'])
 
-leavingAirport = 'edi'
-destination = random.choice(airports['uk'])
-leavingDate = '2013-11-09'
-inboundDate = '2013-11-16'
- 
-session = helpers.createSession(homeCountry, homeCurrency, leavingAirport, destination, leavingDate, inboundDate=inboundDate)
-response = helpers.getLivePriceResponse(session)
+#destination = random.choice(airports['uk']) 
+#session = helpers.createSession(homeCountry, homeCurrency, leavingAirport, destination, leavingDate, inboundDate=inboundDate)
+#response = helpers.getLivePriceResponse(session)
 
-print session
-print destination + '\n'
-print response['Query']['DestinationPlace']+'\n'
-if (len(response['Itineraries']) != 0 ):
-	print response['Itineraries'][0]['PricingOptions'][0]['Price']
+# print session+'\n'
+# print destination + '\n'
+# print response['Query']['DestinationPlace']+'\n'
+failed = True
+while (failed):
+	destination = random.choice(airports['uk'])
+	print 'destination IATA code ' + destination
+	session = helpers.createSession(homeCountry, homeCurrency, leavingAirport, destination, leavingDate, inboundDate=inboundDate)
+	response = helpers.getLivePriceResponse(session)
+	print 'destination id ' + response['Query']['DestinationPlace']
+	failed = len(response['Itineraries']) == 0 
+
+print 'destination code: %s \n destination id: %s \n price: %s \n' % (destination, response['Query']['DestinationPlace'], response['Itineraries'][0]['PricingOptions'][0]['Price'] )
+# if (len(response['Itineraries']) != 0 ):
+# 	print response['Itineraries'][0]['PricingOptions'][0]['Price']
+# else:
+# 	print 'no flights available'
